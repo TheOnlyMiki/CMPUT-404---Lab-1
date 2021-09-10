0beb7d73f552be73881b2bd1784b1f92a0d4c56a
TheOnlyMiki <60678561+TheOnlyMiki@users.noreply.github.com>
2021-09-10 14:21:34 -0600

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -4,5 +4,5 @@ print( "The requests library version:", requests.__version__ )
 
 print( "GET the google homepage:", requests.get( 'http://www.google.com/' ) )
 
-print( "Print my script on Github:", requests.get( 'https://raw.githubusercontent.com/TheOnlyMiki/CMPUT-404---Lab-1/main/lab1.py' ).text )
+print( "Print my script on Github:\n", requests.get( 'https://raw.githubusercontent.com/TheOnlyMiki/CMPUT-404---Lab-1/main/lab1.py' ).text )
 
